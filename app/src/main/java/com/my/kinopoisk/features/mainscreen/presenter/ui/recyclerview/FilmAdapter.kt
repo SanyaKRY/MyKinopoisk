8a1f5aa634fe53c4ab2f83ab2dc8368fdd4a3632
Aliaksandr Kreyer <aliaksandr_kreyer@epam.com>
2024-06-08 20:55:13 +0300

    Add a draft 1.0.0

---
+++ app/src/main/java/com/my/kinopoisk/features/mainscreen/presenter/ui/recyclerview/FilmAdapter.kt
@@ -0,0 +1,4 @@
+package com.my.kinopoisk.features.mainscreen.presenter.ui.recyclerview
+
+class FilmAdapter {
+}
---
+++ app/src/main/java/com/my/kinopoisk/features/mainscreen/presenter/ui/recyclerview/ViewHolderFilm.kt
@@ -0,0 +1,8 @@
+package com.my.kinopoisk.features.mainscreen.presenter.ui.recyclerview
+
+import androidx.recyclerview.widget.RecyclerView
+import com.my.kinopoisk.databinding.FilmItemBinding
+
+class ViewHolderFilm(private val binding: FilmItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+}
