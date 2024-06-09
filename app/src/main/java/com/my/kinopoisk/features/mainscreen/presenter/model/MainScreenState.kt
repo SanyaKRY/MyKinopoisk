40f7a13ac97d8149b78e2252d554a78c515a0b4a
Aliaksandr Kreyer <aliaksandr_kreyer@epam.com>
2024-06-09 18:10:08 +0300

    Add a draft 1.0.6

---
+++ app/src/main/java/com/my/kinopoisk/features/mainscreen/presenter/model/MainScreenState.kt
@@ -0,0 +1,9 @@
+package com.my.kinopoisk.features.mainscreen.presenter.model
+
+sealed interface MainScreenState {
+
+    data object Initial : MainScreenState
+    data object Loading : MainScreenState
+    data object Error : MainScreenState
+    data class Dataloaded(val items: List<FilmUi>) : MainScreenState
+}
--- app/src/main/java/com/my/kinopoisk/features/mainscreen/presenter/vm/MainScreenViewModel.kt
+++ app/src/main/java/com/my/kinopoisk/features/mainscreen/presenter/vm/MainScreenViewModel.kt
@@ -2,10 +2,18 @@ package com.my.kinopoisk.features.mainscreen.presenter.vm
 
 import androidx.lifecycle.ViewModel
 import com.my.kinopoisk.features.mainscreen.domain.usecase.GetListOfFilmsUseCase
+import com.my.kinopoisk.features.mainscreen.presenter.model.MainScreenState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 
 class MainScreenViewModel @Inject constructor(
     private val getListOfFilmsUseCase: GetListOfFilmsUseCase
 ) : ViewModel() {
+
+    private val _stateFlow: MutableStateFlow<MainScreenState> =
+        MutableStateFlow(MainScreenState.Initial)
+    val stateFlow: Flow<MainScreenState>
+        get() = _stateFlow
 
 }
