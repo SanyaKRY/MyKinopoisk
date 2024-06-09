2fd381d4d4e1089816793116e48bd175254d582f
Aliaksandr Kreyer <aliaksandr_kreyer@epam.com>
2024-06-09 17:10:23 +0300

    Add a draft 1.0.5

--- app/src/main/java/com/my/kinopoisk/features/mainscreen/presenter/ui/recyclerview/ViewHolderFilm.kt
+++ app/src/main/java/com/my/kinopoisk/features/mainscreen/presenter/ui/recyclerview/ViewHolderFilm.kt
@@ -3,6 +3,7 @@ package com.my.kinopoisk.features.mainscreen.presenter.ui.recyclerview
 import androidx.recyclerview.widget.RecyclerView
 import com.my.kinopoisk.databinding.FilmItemBinding
 import com.my.kinopoisk.features.mainscreen.presenter.model.FilmUi
+import com.my.kinopoisk.util.extensions.loadImage
 
 class ViewHolderFilm(private val binding: FilmItemBinding) : RecyclerView.ViewHolder(binding.root) {
 
@@ -12,5 +13,6 @@ class ViewHolderFilm(private val binding: FilmItemBinding) : RecyclerView.ViewHo
         val mainGenre = filmUi.genres[0]
         val filmGenreAndYear = "$mainGenre ($year)"
         binding.filmGenreAndYear.text = filmGenreAndYear
+        binding.filmImage.loadImage(filmUi.posterUrlPreview)
     }
 }
--- app/src/main/java/com/my/kinopoisk/util/extensions/UiExtensions.kt
+++ app/src/main/java/com/my/kinopoisk/util/extensions/UiExtensions.kt
@@ -1,2 +1,13 @@
 package com.my.kinopoisk.util.extensions
 
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.my.kinopoisk.R
+
+fun ImageView.loadImage(imageUri: String) {
+    Glide.with(context)
+        .load(imageUri)
+        .placeholder(R.drawable.default_image)
+        .error(R.drawable.default_image)
+        .into(this)
+}
