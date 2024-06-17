1f4cff02c30b68fb1b7cd5cde74e0fb5c6630f60
Aliaksandr Kreyer <aliaksandr_kreyer@epam.com>
2024-06-17 17:36:46 +0300

    Add a draft 1.0.17

--- app/src/main/java/com/my/kinopoisk/features/favouritefilmscreen/presenter/ui/FavouriteFilmScreenFragment.kt
+++ app/src/main/java/com/my/kinopoisk/features/favouritefilmscreen/presenter/ui/FavouriteFilmScreenFragment.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.my.kinopoisk.R
 import com.my.kinopoisk.databinding.FragmentFavouriteFilmScreenBinding
 import com.my.kinopoisk.features.favouritefilmscreen.presenter.model.FavoriteScreenState
 import com.my.kinopoisk.features.favouritefilmscreen.presenter.ui.recyclerview.FavoriteFilmAdapter
@@ -42,6 +50,44 @@ class FavouriteFilmScreenFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         setUpRecyclerView()
         observerFlow()
+        setUpToolBar()
+    }
+
+    private fun setUpToolBar() {
+        (requireActivity() as AppCompatActivity).setSupportActionBar(binding.toolbar)
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.toolbar_menu, menu)
+            }
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.action_search -> {
+                        val searchView = menuItem.actionView as? SearchView
+                        searchView?.queryHint = "My queryHint"
+                        searchView?.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+                            override fun onQueryTextSubmit(p0: String?): Boolean {
+                                searchView.clearFocus()
+                                return true
+                            }
+                            override fun onQueryTextChange(newText: String?): Boolean {
+                                if (newText != null) {
+                                    searchFavoriteFilm(newText)
+                                }
+                                return true
+                            }
+                        })
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+
+    fun searchFavoriteFilm(query: String) {
+        val searchQuery = "%$query%"
+        Log.d("dfsdfsdf","AAA: $searchQuery")
     }
 
     private fun setUpRecyclerView() {
