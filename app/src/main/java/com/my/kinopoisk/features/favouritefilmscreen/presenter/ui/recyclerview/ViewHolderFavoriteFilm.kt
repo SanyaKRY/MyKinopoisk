c51d4d4ce60c12728785a3d69d19fb3e0d22ac34
Aliaksandr Kreyer <aliaksandr_kreyer@epam.com>
2024-06-17 16:39:59 +0300

    Add a draft 1.0.16

--- app/src/main/java/com/my/kinopoisk/features/favouritefilmscreen/presenter/ui/recyclerview/ViewHolderFavoriteFilm.kt
+++ app/src/main/java/com/my/kinopoisk/features/favouritefilmscreen/presenter/ui/recyclerview/ViewHolderFavoriteFilm.kt
@@ -1,8 +1,6 @@
 package com.my.kinopoisk.features.favouritefilmscreen.presenter.ui.recyclerview
 
-import android.view.View
 import androidx.recyclerview.widget.RecyclerView
-import com.my.kinopoisk.R
 import com.my.kinopoisk.databinding.FilmItemBinding
 import com.my.kinopoisk.features.favouritefilmscreen.presenter.model.FavoriteFilmUi
 import com.my.kinopoisk.util.extensions.loadImage
@@ -18,7 +16,5 @@ class ViewHolderFavoriteFilm(
         val filmGenreAndYear = "$genres ($year)"
         binding.filmGenreAndYear.text = filmGenreAndYear
         binding.filmImage.loadImage(filmUi.posterUrl)
-        binding.mark.visibility = View.VISIBLE
-        binding.mark.setImageResource(R.drawable.hand_like)
     }
 }
